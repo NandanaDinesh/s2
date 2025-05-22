908cc495fb8ebf94479b0d037774c0a8ccb77a95
NandanaDinesh <nandanadinesh927@gmail.com>
2025-05-22 14:37:35 +0530

    Create rename.sh

---
+++ shell/rename.sh
@@ -0,0 +1,19 @@
+Q:Shell script to rename all ordinary files in a directory with filename.PID of shell
+
+if [ $# -gt 1 ] 
+then 
+echo "Syntax is <$0> <location> or <$0>" 
+exit 1 
+fi 
+if [ $# -ne 0 ] 
+then 
+cd $1 
+fi 
+for i in * 
+do 
+        if [ -f $i ] 
+        then 
+                mv $i $i.$$ 
+                echo "file renamed to .$$ " 
+        fi 
+done
